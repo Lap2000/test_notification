c8024a18b6dcf085aa8016b5cd187d22e2544456
LapPD <lappd@tng-soft.com.vn>
2022-09-13 09:46:25 +0700

    c

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,7 +1,4 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
-
-import 'noti_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -17,34 +14,34 @@ class _HomePageState extends State<HomePage> {
     // TODO: implement initState
     super.initState();
 
-    LocalNotificationService.initilize();
+    //LocalNotificationService.initilize();
 
-    // Terminated State
-    FirebaseMessaging.instance.getInitialMessage().then((event) {
-      if (event != null) {
-        setState(() {
-          notificationMsg =
-              "${event.notification!.title} ${event.notification!.body} I am coming from terminated state";
-        });
-      }
-    });
-
-    // Foregrand State
-    FirebaseMessaging.onMessage.listen((event) {
-      LocalNotificationService.showNotificationOnForeground(event);
-      setState(() {
-        notificationMsg =
-            "${event.notification!.title} ${event.notification!.body} I am coming from foreground";
-      });
-    });
-
-    // background State
-    FirebaseMessaging.onMessageOpenedApp.listen((event) {
-      setState(() {
-        notificationMsg =
-            "${event.notification!.title} ${event.notification!.body} I am coming from background";
-      });
-    });
+    // // Terminated State
+    // FirebaseMessaging.instance.getInitialMessage().then((event) {
+    //   if (event != null) {
+    //     setState(() {
+    //       notificationMsg =
+    //           "${event.notification!.title} ${event.notification!.body} I am coming from terminated state";
+    //     });
+    //   }
+    // });
+    //
+    // // Foregrand State
+    // FirebaseMessaging.onMessage.listen((event) {
+    //   LocalNotificationService.showNotificationOnForeground(event);
+    //   setState(() {
+    //     notificationMsg =
+    //         "${event.notification!.title} ${event.notification!.body} I am coming from foreground";
+    //   });
+    // });
+    //
+    // // background State
+    // FirebaseMessaging.onMessageOpenedApp.listen((event) {
+    //   setState(() {
+    //     notificationMsg =
+    //         "${event.notification!.title} ${event.notification!.body} I am coming from background";
+    //   });
+    // });
   }
 
   @override
